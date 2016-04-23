067a8a230303346e62dac7590cdfc19aeab65a44
Sean Stuckless <sean.stuckless@gmail.com>
2016-04-23 17:03:38 -0400

    added sagetv unraid

--- sagetv-server/run-server.sh
+++ sagetv-server/run-server.sh
@@ -2,6 +2,7 @@
 
 docker run -d --name sagetv-server \
   -v /home/seans/docker/sagetv-server/MEDIA:/var/media \
+  -v /home/seans/docker/sagetv-server/MEDIAEXT:/var/mediaext \
   -v /home/seans/docker/sagetv-server/SAGETV/:/opt/sagetv \
   -p 42024:42024 -p 7818:7818 -p 8270:8270/udp -p 31100:31100/udp -p 31099:31099 \
   -p 16867:16867/udp -p 16869:16869/udp -p 16881:16881/udp \
