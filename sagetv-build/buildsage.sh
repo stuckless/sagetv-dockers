bd55785699e4f9655f5e05dfb8d783aeb5dcb877
Sean Stuckless <sean.stuckless@gmail.com>
2016-11-20 19:17:18 -0500

    Added features to make the builds more flexible Updated to java8 (java7 was having issues)

--- sagetv-build/buildsage.sh
+++ sagetv-build/buildsage.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-cd /build
+BUILD_DIR=${BUILD_DIR:-/build}
+SAGETV_REPO=${SAGETV_REPO:-https://github.com/google/sagetv.git}
+PGID=${PGID:-1000}
+PUID=${PUID:-1000}
+
+cd $BUILD_DIR
 rm -rf sagetv
 git clone $SAGETV_REPO
+
+if [ -e './build-pre.sh' ] ; then
+   . ./build-pre.sh || exit 1
+fi
+
 cd sagetv/build
 ./buildall.sh
+
+cd ../../
+
+if [ -e './build-post.sh' ] ; then
+   . ./build-post.sh
+fi
+
+echo "Setting persmissions..."
+chown -R $PUID:$PGID sagetv
--- sagetv-build/runit.sh
+++ sagetv-build/runit.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+docker rm sagetv-build
 docker run --name sagetv-build -t -v `pwd`/SOURCES:/build stuckless/sagetv-build
