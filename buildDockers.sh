5cd1508b94f2be4ced8081dfec1b3b73b55252b2
Sean Stuckless <sean.stuckless@gmail.com>
2019-02-10 14:39:11 -0500

    added java 11

--- buildDockers.sh
+++ buildDockers.sh
@@ -6,11 +6,12 @@ if [ "$1" = "" ] ; then
     # cp /tmp/Comskip/comskip ./sagetv-base/
     # chmod 775 sagetv-base/comskip
     # stop if any fail
-    docker build -t stuckless/sagetv-base:latest sagetv-base/ && \
+    #docker build -t stuckless/sagetv-base:latest sagetv-base/ && \
     #docker build -t stuckless/sagetv-server-java7:latest sagetv-server-java7/ && \
-    docker build -t stuckless/sagetv-server-java8:latest sagetv-server-java8/ && \
-    docker build -t stuckless/sagetv-server-java9:latest sagetv-server-java9/ && \
-    docker build -t stuckless/sagetv-server-java10:latest sagetv-server-java10/
+    #docker build -t stuckless/sagetv-server-java8:latest sagetv-server-java8/ && \
+    #docker build -t stuckless/sagetv-server-java9:latest sagetv-server-java9/ && \
+    #docker build -t stuckless/sagetv-server-java10:latest sagetv-server-java10/
+    docker build -t stuckless/sagetv-server-java11:latest sagetv-server-java11/
     # docker build -t stuckless/sagetv-server-beta:latest sagetv-server-beta/
     # docker build -t stuckless/sagetv-server-beta:latest sagetv-server-beta/
 else
--- pushDockers.sh
+++ pushDockers.sh
@@ -2,9 +2,10 @@
 
 docker login --username=stuckless --email=sean.stuckless@gmail.com
 
-docker push stuckless/sagetv-base
+#docker push stuckless/sagetv-base
 #docker push stuckless/sagetv-server-java7
-docker push stuckless/sagetv-server-java8
-docker push stuckless/sagetv-server-java9
-docker push stuckless/sagetv-server-java10
+#docker push stuckless/sagetv-server-java8
+#docker push stuckless/sagetv-server-java9
+#docker push stuckless/sagetv-server-java10
+docker push stuckless/sagetv-server-java11
 #docker push stuckless/sagetv-server-beta
